eef607c168668f813a612d3c2885098ea9f5ba64
Tales Andrade <andradetales5@gmail.com>
2021-07-09 19:24:54 -0400

    feat: adding a game to favorites list

--- src/controllers/favorites.js
+++ src/controllers/favorites.js
@@ -1,12 +1,17 @@
 const FavoriteModel = require('../models/favorite');
 
 module.exports.renderMain = async (req, res, next) => {
-    const favorites = await FavoriteModel.find({ user: req.session.user.id });
+    const favorites = await FavoriteModel.findOne({ user: req.params.id });
+    console.log(favorites.games);
     res.render('games/favorites', { favorites });
 };
 
-module.exports.addOneFavorite = (req, res) => {
-    console.log("testing");
+module.exports.addOneFavorite = async (req, res, next) => {
+    const favorites = await FavoriteModel.findOne({ user: req.params.id });
+    favorites.games.push(req.body.game);
+    await favorites.save();
+
+    res.redirect(`/profiles/${req.params.id}/favorites`);
 };
 
 module.exports.deleteOneFavorite = (req, res) => {
